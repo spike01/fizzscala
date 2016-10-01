c9b92e6a5342a1f769a7f45d3447688c5431088f
Spike Lindsey <spike01@gmail.com>
2016-10-01 19:41:19 +0100

    Refactor to use matchers

--- src/test/scala/FizzBuzzSpec.scala
+++ src/test/scala/FizzBuzzSpec.scala
@@ -3,26 +3,26 @@ import org.scalatest.{FlatSpec, Matchers}
 class FizzBuzzSpec extends FlatSpec with Matchers {
   it should "fizz for multiples of three" in {
     val fizzbuzz = new FizzBuzz()
-    assert(fizzbuzz.play(3) === "Fizz")
-    assert(fizzbuzz.play(6) === "Fizz")
+    fizzbuzz.play(3) shouldBe "Fizz"
+    fizzbuzz.play(6) shouldBe "Fizz"
   }
 
   it should "buzz for multiples of five" in {
     val fizzbuzz = new FizzBuzz()
-    assert(fizzbuzz.play(5) === "Buzz")
-    assert(fizzbuzz.play(10) === "Buzz")
+    fizzbuzz.play(5) shouldBe "Buzz"
+    fizzbuzz.play(10) shouldBe "Buzz"
   }
 
   it should "fizzbuzz for multiples of fifteen" in {
     val fizzbuzz = new FizzBuzz()
-    assert(fizzbuzz.play(15) === "FizzBuzz")
-    assert(fizzbuzz.play(30) === "FizzBuzz")
+    fizzbuzz.play(15) shouldBe "FizzBuzz"
+    fizzbuzz.play(30) shouldBe "FizzBuzz"
   }
 
   it should "pass the number through (as a string) otherwise" in {
     val fizzbuzz = new FizzBuzz()
-    assert(fizzbuzz.play(1) === "1")
-    assert(fizzbuzz.play(7) === "7")
-    assert(fizzbuzz.play(28) === "28")
+    fizzbuzz.play(1) shouldBe "1"
+    fizzbuzz.play(7) shouldBe "7"
+    fizzbuzz.play(28) shouldBe "28"
   }
 }
